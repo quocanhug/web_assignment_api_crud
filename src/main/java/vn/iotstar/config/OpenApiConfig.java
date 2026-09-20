fc5b938c416b2ea4cab889b9cdfc2e1f47d359d0
quocanhug <quocanhug@gmail.com>
2026-09-20 22:16:40 +0700

    feat: configure Swagger 3 (OpenAPI 3.0) on Spring Boot with port 8082

--- src/main/java/vn/iotstar/config/OpenApiConfig.java
+++ src/main/java/vn/iotstar/config/OpenApiConfig.java
@@ -1,11 +1,24 @@
 package vn.iotstar.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
 @Configuration
 public class OpenApiConfig implements WebMvcConfigurer {
+
+	@Bean
+	public OpenAPI customOpenAPI() {
+		return new OpenAPI()
+				.info(new Info()
+						.title("RESTful API Category & Product Management")
+						.version("v1.0.0")
+						.description("CRUD API Category và Product"));
+	}
 
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
